7061f48b9aeef8eb37dc3b34f2e1b564c91ba584
Mark Baggett <mbagget1@utk.edu>
2018-04-16 09:18:34 -0400

    Add some more exception handling.

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import yaml
 from time import sleep
+from urllib3.exceptions import ProtocolError
 
 
 # Add stuff we'll need later
@@ -36,6 +37,10 @@ class SelectedWork:
                 print(f"Scraping metadata for {self.title} at {directory}.")
                 with open(f"{save_path}/{directory}/metadata.txt", "w") as metadata:
                     metadata.write(work_details.text)
+            except (ProtocolError, AttributeError):
+                with open(f"error.log", "a") as error_log:
+                    error_log.write(f"Connection refused with {self.statuscode} on metadata"
+                                    f" for {directory} aka {self.title}.")
             except:
                 with open(f"error.log", "a") as error_log:
                     error_log.write(f"Connection refused with {self.statuscode} on metadata"
@@ -46,24 +51,32 @@ class SelectedWork:
 
     def grab_pdf(self, directory):
         r = requests.get(f"{self.url}/download/")
-        try:
-            if r.status_code == 200:
+        if r.headers["Content-Type"] == "application/pdf; charset=utf-8":
+            try:
+                if r.status_code == 200:
+                    self.statuscode = r.status_code
+                    print(f"Downloading PDF for {self.title} at {directory}.")
+                    with open(f"{save_path}/{directory}/stamped.pdf", 'wb') as work:
+                        work.write(r.content)
+            except requests.exceptions.ConnectionError:
                 self.statuscode = r.status_code
-                print(f"Downloading PDF for {self.title} at {directory}.")
-                with open(f"{save_path}/{directory}/stamped.pdf", 'wb') as work:
-                    work.write(r.content)
-        except requests.exceptions.ConnectionError:
-            self.statuscode = r.status_code
+                with open(f"error.log", "a") as error_log:
+                    error_log.write(f"Connection refused with {r.status_code} on pdf for {directory} aka {self.title}.")
+                sleep(3)
+                self.grab_pdf(directory)
+            except requests.ConnectionError as e:
+                self.statuscode = r.status_code
+                with open("error.log", "a") as error_log:
+                    error_log.write(f"Failed as {e}. Connection refused with {r.status_code} on pdf for {directory} aka {self.title}.")
+        else:
             with open(f"error.log", "a") as error_log:
-                error_log.write(f"Connection refused with {r.status_code} on pdf for {directory} aka {self.title}.")
-            sleep(3)
-            self.grab_pdf(directory)
-
+                error_log.write(f"No PDF for {directory}.\n")
         return
 
     def create_container(self):
         dir_name = self.url.replace("http://works.bepress.com/", "").replace("/", "__")
         try:
+            print(f"Creating directory {dir_name}.")
             os.mkdir(f"{save_path}/{dir_name}")
             return dir_name
         except:
